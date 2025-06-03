ee39a723b24f7cf4fe119b23d78e7a26ff49f2df
Jonathan Hesler <jeh8443@rit.edu>
2025-06-02 22:40:42 -0400

    Empty App/Docs Folder

---
+++ App/App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Jonathan Hesler on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
